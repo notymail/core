b983ca3c3aaeb70677b2252dc4bbb572614206b8
Dafnik <git@dafnik.me>
2024-06-24 10:05:12 +0200

    refactor: add 10 seconds timeout to nodemailer

--- apps/api/src/utils/mail.ts
+++ apps/api/src/utils/mail.ts
@@ -1,5 +1,7 @@
 import * as nodemailer from 'nodemailer';
 
+const TEN_SECONDS_MS = 10 * 1000;
+
 export default function sendMail(
   to: string | string[],
   subject: string,
@@ -15,6 +17,10 @@ export default function sendMail(
         user: process.env.MAIL_AUTH_USER,
         pass: process.env.MAIL_AUTH_PASSWORD,
       },
+      connectionTimeout: TEN_SECONDS_MS,
+      greetingTimeout: TEN_SECONDS_MS,
+      socketTimeout: TEN_SECONDS_MS,
+      dnsTimeout: TEN_SECONDS_MS,
     })
     .sendMail({
       from: process.env.MAIL_SENDER,
